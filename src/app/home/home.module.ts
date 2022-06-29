58eb68a914c6d625df8ed50996e7eae3a3b3ff7d
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2022-06-29 10:17:51 -0300

    create base home routings

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { Routes } from '@angular/router';
+import { FULL, HOME_ROUTING, NAME_PROJECT } from '../shared/http.routes.consts';
+
+export const routes: Routes = [
+  {path: '', redirectTo: `${NAME_PROJECT}`, pathMatch: FULL},
+  {
+    path: NAME_PROJECT,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        children: [
+          {path: '', redirectTo: `${HOME_ROUTING}`, pathMatch: FULL},
+          {
+            path: HOME_ROUTING,
+            loadChildren: () => import('./home.module').then(m => m.HomeModule),
+            data: {}
+          },
+        ]
+      }
+    ]
+  },
+];
 
 @NgModule({
   declarations: [
--- src/app/shared/http.routes.consts.ts
+++ src/app/shared/http.routes.consts.ts
@@ -1,6 +1,11 @@
 import { environment } from '../../environments/environment';
 
-export const API = 'api';
-export const NAME_PROJECT = 'blog'
-export const VERSION_PROJECT = 'V1'
-export const BACKEND_URL = `http://${environment.BASE_URL}/${API}/${VERSION_PROJECT}`;
+export const FULL = 'full';
+
+export const API: string = 'api';
+export const NAME_PROJECT: string = 'blog'
+export const VERSION_PROJECT: string = 'V1'
+export const BACKEND_URL: string = `http://${environment.BASE_URL}/${API}/${VERSION_PROJECT}`;
+
+
+export const HOME_ROUTING: string = 'home';
